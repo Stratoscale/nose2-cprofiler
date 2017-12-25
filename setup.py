09aa4d91b8e8ebdc14bdfd09116d802a11b12cb9
Noam Chadash <noam@stratoscale.com>
2017-12-25 19:27:17 +0200

    Adding 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nose2-cprofiler-plugin',
-    version='1.0.0',
+    version='1.0.1',
     description='cprofiler plugin for nose2',
     url='https://github.com/stratoscale/nose2-cprofiler-plugin',
     author='Noam Chadash',
