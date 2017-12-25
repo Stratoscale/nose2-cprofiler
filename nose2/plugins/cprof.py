0f03480db66e25480a87f614d181520105a9f0de
Noam Chadash <noam@stratoscale.com>
2017-12-25 18:36:32 +0200

    Adding restricitions to cprofiler usage

--- nose2/plugins/cprof.py
+++ nose2/plugins/cprof.py
@@ -16,7 +16,7 @@ log = logging.getLogger('.'.join(('nose2', 'plugins', __package__)))
 
 class Profiler(nose2.events.Plugin):
 
-    '''Profile the test run using cProfile'''
+    """Profile the test run using cProfile"""
 
     configSection = 'profiler'
     commandLineSwitch = ('P', 'profile', 'Run test under cprofile')
@@ -24,19 +24,19 @@ class Profiler(nose2.events.Plugin):
     def __init__(self):
         self.sort = self.config.as_str('sort', 'cumulative')
         self.report_filename = self.config.as_str('filename', 'reports/profiler.prof')
-        self.count_of_methods_to_print = int(self.config.as_str('count_of_methods_to_print', '20'))
+        self.restrictions = (self.config.as_str('regex', []), self.config.as_int('count_of_lines_to_print', []))
         self.prof = None
 
     def startTestRun(self, event):
-        '''Setup profiler'''
+        """Setup profiler"""
         self.prof = cProfile.Profile()
         event.executeTests = self.prof.runcall
 
     def beforeSummaryReport(self, event):
-        '''Output profiling results'''
+        """Output profiling results"""
         self.prof.disable()
         stats = pstats.Stats(self.prof, stream=event.stream).sort_stats(
             self.sort)
         event.stream.writeln(nose2.util.ln('Profiling results'))
         stats.dump_stats(self.report_filename)
-        stats.print_stats(self.count_of_methods_to_print)
+        stats.print_stats(*self.restrictions)
